bbf68a6b9a6df50a48b1c3cd9a68f0bc182aef70
leebuwon <bw1611@naver.com>
2024-06-30 22:36:15 +0900

    data sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,54 @@
 DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS delivery;
+DROP TABLE IF EXISTS delivery_log;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS coupon;
 
 CREATE TABLE accounts (
-    id bigint auto_increment primary key,
-    email varchar(255) unique not null,
-    password varchar(255) not null,
-    city varchar(255) not null,
-    region varchar(255) not null,
-    zip varchar(255) not null,
-    created_at timestamp,
-    modified_at timestamp
+                          id bigint auto_increment primary key,
+                          email varchar(255) unique not null,
+                          password varchar(255) not null,
+                          password_expiration_date timestamp,
+                          password_failed_count INT NOT NULL,
+                          password_ttl BIGINT,
+                          city varchar(255) not null,
+                          region varchar(255) not null,
+                          zip varchar(255) not null,
+                          created_at timestamp,
+                          modified_at timestamp
+);
+
+CREATE TABLE coupons (
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         discount_amount DOUBLE NOT NULL,
+                         `use` BOOLEAN NOT NULL,
+                         created_at timestamp,
+                         modified_at timestamp
+);
+
+CREATE TABLE orders (
+                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        price DOUBLE NOT NULL,
+                        coupon_id BIGINT,
+                        created_at timestamp,
+                        modified_at timestamp,
+                        FOREIGN KEY (coupon_id) REFERENCES coupons(id)
+);
+
+CREATE TABLE delivery (
+                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          city varchar(255) not null,
+                          region varchar(255) not null,
+                          zip varchar(255) not null,
+                          created_at timestamp,
+                          modified_at timestamp
+);
+
+CREATE TABLE delivery_log (
+                              id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                              status VARCHAR(255) NOT NULL,
+                              delivery_id BIGINT NOT NULL,
+                              created_at timestamp,
+                              modified_at timestamp,
+                              FOREIGN KEY (delivery_id) REFERENCES delivery(id)
 );
